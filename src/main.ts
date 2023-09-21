8aeb27231b01a7b61de003ee2c843878d6b48b28
Ben <ben.visintainer@me.com>
2023-09-21 10:15:54 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
 
+interface Version {
+  major: number;
+  minor: number;
+  patch: number;
+  build: number;
+  revision: number;
+  suffix: string;
+}
+
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const version: Version = core.getInput('version-json')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Parsing ${version_json} ...`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('major', version.major)
+    core.setOutput('minor', version.minor)
+    core.setOutput('patch', version.patch)
+    core.setOutput('build', version.build)
+    core.setOutput('revision', version.revision)
+    core.setOutput('suffix', version.suffix)
+    
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
