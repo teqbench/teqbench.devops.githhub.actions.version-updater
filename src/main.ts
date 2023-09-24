a7b38e41e16c946378822c9c133b7758d0b036a3
Ben <ben.visintainer@me.com>
2023-09-23 22:34:36 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,9 @@ export async function run(): Promise<void> {
           // Minor version component is unchanged.
           // Incremment patch/revision.
 
-          version.patch = version.revision++
+          // NOTE: for Trading Toolbox, patch and reversion are the same.
+          version.patch++
+          version.revision = version.patch
           break
         }
       }
@@ -114,64 +116,3 @@ function stringToEnum(value: string): VersionReleaseType | null {
   return null
 }
 
-// async function processVersionJson(
-//   releaseType: VersionReleaseType
-// ): Promise<void> {
-//   try {
-//     const inputVersionJson: string = getInput('version-json')
-//     const version: Version = JSON.parse(inputVersionJson)
-
-//     // NOTE: for Trading Toolbox, patch and reversion are the same.
-//     switch (releaseType) {
-//       case VersionReleaseType.MAJOR: {
-//         // Increment major version component is unchanged.
-//         // Reset minor, patch/revision to 0.
-
-//         version.major++
-//         version.minor = 0
-//         version.patch = version.revision = 0
-
-//         break
-//       }
-//       case VersionReleaseType.MINOR: {
-//         // Major version component is unchanged.
-//         // Increment minor version component.
-//         // Reset patch/revision to 0.
-
-//         version.minor++
-//         version.patch = version.revision = 0
-
-//         break
-//       }
-//       case VersionReleaseType.PATCH: {
-//         // Major version component is unchanged.
-//         // Minor version component is unchanged.
-//         // Incremment patch/revision.
-
-//         version.patch = version.revision++
-//         break
-//       }
-//     }
-
-//     // Build version component always increments.
-//     version.build++
-
-//     // Set outputs for other workflow steps to use
-//     setOutput('major', version.major)
-//     setOutput('minor', version.minor)
-//     setOutput('patch', version.patch)
-//     setOutput('build', version.build)
-//     setOutput('revision', version.revision)
-//     setOutput('suffix', version.suffix)
-
-//     const versionString = `${version.major}.${version.minor}.${version.build}.${version.revision}`
-
-//     setOutput('version', versionString)
-//     setOutput('tag', `v${versionString}`)
-//   } catch (error) {
-//     // Fail the workflow run if an error occurs
-//     if (error instanceof Error) {
-//       setFailed('Invalid version json')
-//     }
-//   }
-// }
